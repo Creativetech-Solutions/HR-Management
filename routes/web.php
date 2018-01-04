ca9fddde9c952c077d2a4c73f01b322ef6b424d3
mudassirmir <mudassirmir25@gmail.com>
2018-01-04 18:29:42 +0500

    milestone

--- routes/web.php
+++ routes/web.php
@@ -154,3 +154,25 @@ Route::get('settings/logo', 'Settings\Setting@logo');
 Route::post('/settings/update/{id}','Settings\Setting@update');
 
 
+
+// ****************  Milestones  *****************************
+
+//   Add
+Route::get('/milestones/add', 'Milestones\Milestones@add');
+Route::post('/milestones/store','Milestones\Milestones@store');
+//   edit
+Route::get('/milestones/edit/{id}', ['as'=>'milestones.edit','uses'=>'Milestones\Milestones@edit']);     // use like this when url send from view
+Route::post('/milestones/update/{id}','Milestones\Milestones@update');
+//   View
+Route::get('/milestones', 'Milestones\Milestones@index');
+Route::get('/get-data-milestones', ['as'=>'get.milestones_data','uses'=>'Milestones\Milestones@getData']);
+//get data for single employee and client
+Route::get('/get-data-getData_me/{id}', ['as'=>'get.milestones_data_me','uses'=>'Milestones\Milestones@getData_me']);
+Route::get('/get-data-getData_for_cl/{id}', ['as'=>'get.getData_for_cl','uses'=>'Milestones\Milestones@getData_for_cl']);
+//   delete
+Route::delete('/milestones_delete/{id}', ['as'=>'milestones_delete','uses'=>'Milestones\Milestones@destroy']);
+//   Change Status
+Route::post('/milestones_status/', ['as'=>'milestones_status','uses'=>'Milestones\Milestones@change_status']);
+Route::post('/milestones_pay_status/', ['as'=>'milestones_pay_status','uses'=>'Milestones\Milestones@change_payment_status']);
+Route::POST('/check_milestones_name', ['as'=>'check_milestones_name','uses'=>'Milestones\Milestones@check_Milestones_name']);
+
